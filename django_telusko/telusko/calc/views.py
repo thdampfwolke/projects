4d34654688efa5e1ac8460b6e8b77d3088379ac0
th dampwolke <thdampfwolke@gmail.com>
2021-03-23 22:19:57 +0100

    added:

--- django_telusko/telusko/calc/views.py
+++ django_telusko/telusko/calc/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse("Hello World")
-    return render(request, 'home.html')
+    # return render(request, 'home.html')
+    return render(request, 'home.html', {'name':'abc'})
