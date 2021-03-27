c966f13b68e459037024bfc316f1f0b9a1b12f68
th. dampfwolke <thdampfwolke@gmail.com>
2021-03-27 18:34:58 +0100

    added: 013

--- django_telusko/telusko/travello/views.py
+++ django_telusko/telusko/travello/views.py
@@ -6,20 +6,22 @@ from .models import Destination
 def index(request):
     dest1 = Destination()
     dest1.name = 'dest-1: Mumbai'
-    # dest1.img = ''
+    dest1.img = 'destination_1.jpg'
     dest1.desc = 'desc: dest-1: mumbai: city'
     dest1.price = '711'
 
     dest2 = Destination()
     dest2.name = 'dest-2: Mumbai'
-    # dest2.img = ''
+    dest2.img = 'destination_2.jpg'
     dest2.desc = 'desc: dest-2: mumbai: city'
     dest2.price = '722'
 
     dest3 = Destination()
     dest3.name = 'dest-3: Mumbai'
-    # dest3.img = ''
+    dest3.img = 'destination_4.jpg'
     dest3.desc = 'desc: dest-3: mumbai: city'
     dest3.price = '733'
 
-    return render(request, 'index.html', {'dest1': dest1, 'dest2': dest2, 'dest3': dest3})
+    dests = [dest1, dest2, dest3]
+
+    return render(request, 'index.html', {'dests': dests})
