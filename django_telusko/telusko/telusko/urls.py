f213eaca440af50077048dcfed044368cf12044e
th. dampfwolke <thdampfwolke@gmail.com>
2021-03-30 17:35:18 +0200

    added:

--- django_telusko/telusko/telusko/urls.py
+++ django_telusko/telusko/telusko/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # path('', include('calc.urls')),
     path('', include('travello.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
