6a1672bac4fd6a6c9e7b9011ffcd997cc3adb085
th. dampfwolke <thdampfwolke@gmail.com>
2021-04-01 16:19:03 +0200

    added:

--- django_telusko/telusko/accounts/views.py
+++ django_telusko/telusko/accounts/views.py
@@ -2,25 +2,24 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
-# print('--> err: Username taken')
 
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-#def login(request):
-#    if request.method== 'POST':
-#        username = request.POST['username']
-#        password = request.POST['password']
-#
-#        user = auth.authenticate(username=username,password=password)
-#
-#        if user is not None:
-#            auth.login(request, user)
-#            return redirect("/")
-#        else:
-#            messages.info(request,'invalid credentials')
-#            return redirect('login')
-#
-#    else:
-#        return render(request,'login.html')
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            print('--> err: invalid credentials')
+            messages.info(request,'--> err: invalid credentials')
+            return redirect('login')
+
+    else:
+        return render(request,'login.html')
 
 
 def register(request):
@@ -46,8 +45,7 @@ def register(request):
                 user = User.objects.create_user(username=username, password=password1, email=email,first_name=first_name,last_name=last_name)
                 user.save();
                 print('--> ok:  user created')
-                return redirect('/')
-                #return redirect('login')
+                return redirect('login')
 
         else:
             print('--> err: password not matching..')
@@ -59,6 +57,6 @@ def register(request):
         return render(request,'register.html')
 
 
-#def logout(request):
-#    auth.logout(request)
-#    return redirect('/')
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
