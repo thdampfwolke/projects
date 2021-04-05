f69646a2ddeb3f680c2b2922ff2324eddefbe19f
th. dampfwolke <thdampfwolke@gmail.com>
2021-04-05 12:31:59 +0200

    added:

--- django_sap_02/sap_02/app_staff/views.py
+++ django_sap_02/sap_02/app_staff/views.py
@@ -1,4 +1,9 @@
+# ../app_staff/views.py
+
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+    # return render(request, 'index.html')
+    # return render(request.POST, 'index.html')
+    
