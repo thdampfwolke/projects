3162b30d67b58c49f99e3af0d7fbaff3e8bc95c8
th. dampfwolke <thdampfwolke@gmail.com>
2021-04-10 21:30:45 +0200

    added:

--- django_sap_03/sap_03/app_test/models.py
+++ django_sap_03/sap_03/app_test/models.py
@@ -4,19 +4,21 @@ from django.db import models
 
 class Staff(models.Model):
     # id = int
-    nname          = models.CharField(max_length=120)
-    vname          = models.CharField(max_length=120)
-    geb            = models.DateField()
-    pic            = models.ImageField(upload_to='pics')
-    email          = models.CharField(max_length=254)
-    desc           = models.TextField()
-    check          = models.BooleanField(default=False)
-    is_active      = models.BooleanField(default=True)
-    date_beg       = models.DateField()
-    date_end       = models.DateField()
+    nname          = models.CharField(max_length=120, null=False)
+    vname          = models.CharField(max_length=120, null=False)
+    geb            = models.DateField(null=True, blank=True)
+    pic            = models.ImageField(upload_to='pics', null=True, blank=True)
+    email          = models.CharField(max_length=254, null=True, blank=True)
+    desc           = models.TextField(null=True, blank=True)
+    check          = models.BooleanField(null=False, default=False)
+    is_active      = models.BooleanField(null=False, default=True)
+    date_beg       = models.DateField(null=False)
+    date_end       = models.DateField(null=False, default='2099-12-31')
     date_created   = models.DateTimeField(auto_now_add=True)
     date_modified  = models.DateTimeField(auto_now=True)
 
+
+# (null=True, blank=True)
 
 # class Mitarbeiter2(models.Model):
 #     # id = int
