fbb5b289016f6d90125fea9edbda3b6ed5fbb08e
th. dampfwolke <thdampfwolke@gmail.com>
2021-04-17 21:22:13 +0200

    added:

--- django_sap_03/sap_03/app_test/admin.py
+++ django_sap_03/sap_03/app_test/admin.py
@@ -2,5 +2,7 @@
 
 from django.contrib import admin
 from .models import Staff
+# from .models import Rolle
 
 admin.site.register(Staff)
+# admin.site.register(Rolle)
--- django_sap_03/sap_03/app_test/models.py
+++ django_sap_03/sap_03/app_test/models.py
@@ -2,6 +2,118 @@
 
 from django.db import models
 
+
+# ----------------------------------------------------------------------------
+class Infra_umgebung(models.Model):
+    # id = int
+    name           = models.CharField(max_length=120)
+    beschreibung   = models.TextField(null=True, blank=True)
+    check          = models.BooleanField(null=False, default=False)
+    is_active      = models.BooleanField(null=False, default=True)
+    date_beg       = models.DateField(null=False)
+    date_end       = models.DateField(null=False, default='2099-12-31')
+    date_created   = models.DateTimeField(auto_now_add=True)
+    date_modified  = models.DateTimeField(auto_now=True)
+
+# ----------------------------------------------------------------------------
+class Inventar_art(models.Model):
+    # id = int
+    name           = models.CharField(max_length=120)
+    beschreibung   = models.TextField(null=True, blank=True)
+    check          = models.BooleanField(null=False, default=False)
+    is_active      = models.BooleanField(null=False, default=True)
+    date_beg       = models.DateField(null=False)
+    date_end       = models.DateField(null=False, default='2099-12-31')
+    date_created   = models.DateTimeField(auto_now_add=True)
+    date_modified  = models.DateTimeField(auto_now=True)
+
+# ----------------------------------------------------------------------------
+class Inventar(models.Model):
+    # id = int
+    name           = models.CharField(max_length=120)
+    beschreibung   = models.TextField(null=True, blank=True)
+    inventar_art   = models.ForeignKey(Inventar_art, on_delete=models.CASCADE)
+    check          = models.BooleanField(null=False, default=False)
+    is_active      = models.BooleanField(null=False, default=True)
+    date_beg       = models.DateField(null=False)
+    date_end       = models.DateField(null=False, default='2099-12-31')
+    date_created   = models.DateTimeField(auto_now_add=True)
+    date_modified  = models.DateTimeField(auto_now=True)
+
+# ----------------------------------------------------------------------------
+class Itsk_objekte(models.Model):
+    # id = int
+    name           = models.CharField(max_length=120)
+    beschreibung   = models.TextField(null=True, blank=True)
+    check          = models.BooleanField(null=False, default=False)
+    is_active      = models.BooleanField(null=False, default=True)
+    date_beg       = models.DateField(null=False)
+    date_end       = models.DateField(null=False, default='2099-12-31')
+    date_created   = models.DateTimeField(auto_now_add=True)
+    date_modified  = models.DateTimeField(auto_now=True)
+
+# ----------------------------------------------------------------------------
+class Komponente_art(models.Model):
+    # id = int
+    komponente_art = models.CharField(max_length=120)
+    beschreibung   = models.TextField(null=True, blank=True)
+    check          = models.BooleanField(null=False, default=False)
+    is_active      = models.BooleanField(null=False, default=True)
+    date_beg       = models.DateField(null=False)
+    date_end       = models.DateField(null=False, default='2099-12-31')
+    date_created   = models.DateTimeField(auto_now_add=True)
+    date_modified  = models.DateTimeField(auto_now=True)
+
+# ----------------------------------------------------------------------------
+class Komponente(models.Model):
+    # id = int
+    komponente     = models.CharField(max_length=120)
+    beschreibung   = models.TextField(null=True, blank=True)
+    komponente_art = models.ForeignKey(Komponente_art, on_delete=models.CASCADE)
+    check          = models.BooleanField(null=False, default=False)
+    is_active      = models.BooleanField(null=False, default=True)
+    date_beg       = models.DateField(null=False)
+    date_end       = models.DateField(null=False, default='2099-12-31')
+    date_created   = models.DateTimeField(auto_now_add=True)
+    date_modified  = models.DateTimeField(auto_now=True)
+
+# ----------------------------------------------------------------------------
+class Remedy(models.Model):
+    # id = int
+    remedy         = models.CharField(max_length=120)
+    beschreibung   = models.TextField(null=True, blank=True)
+    check          = models.BooleanField(null=False, default=False)
+    is_active      = models.BooleanField(null=False, default=True)
+    date_beg       = models.DateField(null=False)
+    date_end       = models.DateField(null=False, default='2099-12-31')
+    date_created   = models.DateTimeField(auto_now_add=True)
+    date_modified  = models.DateTimeField(auto_now=True)
+
+# ----------------------------------------------------------------------------
+class Rolle(models.Model):
+    # id = int
+    rolle          = models.CharField(max_length=120)
+    beschreibung   = models.TextField(null=True, blank=True)
+    check          = models.BooleanField(null=False, default=False)
+    is_active      = models.BooleanField(null=False, default=True)
+    date_beg       = models.DateField(null=False)
+    date_end       = models.DateField(null=False, default='2099-12-31')
+    date_created   = models.DateTimeField(auto_now_add=True)
+    date_modified  = models.DateTimeField(auto_now=True)
+
+# ----------------------------------------------------------------------------
+class Sap_system_landschaft(models.Model):
+    # id = int
+    name           = models.CharField(max_length=120)
+    beschreibung   = models.TextField(null=True, blank=True)
+    check          = models.BooleanField(null=False, default=False)
+    is_active      = models.BooleanField(null=False, default=True)
+    date_beg       = models.DateField(null=False)
+    date_end       = models.DateField(null=False, default='2099-12-31')
+    date_created   = models.DateTimeField(auto_now_add=True)
+    date_modified  = models.DateTimeField(auto_now=True)
+
+# ----------------------------------------------------------------------------
 class Staff(models.Model):
     # id = int
     nname          = models.CharField(max_length=120, null=False)
@@ -17,15 +129,28 @@ class Staff(models.Model):
     date_created   = models.DateTimeField(auto_now_add=True)
     date_modified  = models.DateTimeField(auto_now=True)
 
+# ----------------------------------------------------------------------------
+class Standort(models.Model):
+    # id = int
+    standort       = models.CharField(max_length=120)
+    strasse        = models.CharField(max_length=120)
+    raum           = models.CharField(max_length=120)
+    beschreibung   = models.TextField(null=True, blank=True)
+    check          = models.BooleanField(null=False, default=False)
+    is_active      = models.BooleanField(null=False, default=True)
+    date_beg       = models.DateField(null=False)
+    date_end       = models.DateField(null=False, default='2099-12-31')
+    date_created   = models.DateTimeField(auto_now_add=True)
+    date_modified  = models.DateTimeField(auto_now=True)
 
-# (null=True, blank=True)
-
-# class Mitarbeiter2(models.Model):
-#     # id = int
-#     nname = models.CharField(max_length=120)
-#     vname = models.CharField(max_length=120)
-#     img = models.ImageField(upload_to='pics')
-#     desc = models.TextField()
-#     phone = models.IntegerField()
-#     handy = models.IntegerField()
-#     offer = models.BooleanField(default=False)
+# ----------------------------------------------------------------------------
+class Verfahren(models.Model):
+    # id = int
+    verfahren      = models.CharField(max_length=120)
+    beschreibung   = models.TextField(null=True, blank=True)
+    check          = models.BooleanField(null=False, default=False)
+    is_active      = models.BooleanField(null=False, default=True)
+    date_beg       = models.DateField(null=False)
+    date_end       = models.DateField(null=False, default='2099-12-31')
+    date_created   = models.DateTimeField(auto_now_add=True)
+    date_modified  = models.DateTimeField(auto_now=True)
