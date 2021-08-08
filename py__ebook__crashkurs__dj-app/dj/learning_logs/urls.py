3d30d241c3b4c7fd799b9c4684be91e2d1bb6a6a
th. dampfwolke <thdampfwolke@gmail.com>
2021-08-08 11:45:46 +0200

    added:

--- py__ebook__crashkurs__dj-app/dj/learning_logs/urls.py
+++ py__ebook__crashkurs__dj-app/dj/learning_logs/urls.py
@@ -7,6 +7,8 @@ app_name = 'learning_logs'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
 
 
