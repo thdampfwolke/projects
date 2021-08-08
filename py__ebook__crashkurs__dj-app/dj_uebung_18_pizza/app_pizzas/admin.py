f696ed2792a5336db34fc19eb370df2c8c4e3a6d
th dampwolke <thdampfwolke@gmail.com>
2021-08-08 22:35:26 +0200

    added:

--- py__ebook__crashkurs__dj-app/dj_uebung_18_pizza/app_pizzas/admin.py
+++ py__ebook__crashkurs__dj-app/dj_uebung_18_pizza/app_pizzas/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+# ../app_pizza/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Pizza, Topping
+
+admin.site.register(Pizza)
+admin.site.register(Topping)
--- py__ebook__crashkurs__dj-app/dj_uebung_18_pizza/app_pizzas/models.py
+++ py__ebook__crashkurs__dj-app/dj_uebung_18_pizza/app_pizzas/models.py
@@ -1,3 +1,30 @@
+# ../app_pizzas/models.py
+
 from django.db import models
 
-# Create your models here.
+
+class Pizza(models.Model):
+    """pizza: welche soll es sein"""
+    name = models.CharField(max_length=50)
+    preis = models.DecimalField(max_digits=5, decimal_places=2)
+    date_add = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "pizza"
+
+    def __str__(self):
+        return self.name
+
+
+class Topping(models.Model):
+    """topping: welche soll es sein"""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    preis = models.DecimalField(max_digits=5, decimal_places=2)
+    date_add = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "topping"
+
+    def __str__(self):
+        return self.name
