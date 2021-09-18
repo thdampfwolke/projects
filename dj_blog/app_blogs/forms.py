86b011afc7728f39c8baa8c9f8b271cfe17211f5
th dampwolke <thdampfwolke@gmail.com>
2021-09-18 23:00:10 +0200

    added:

---
+++ dj_blog/app_blogs/forms.py
@@ -0,0 +1,69 @@
+# ------------------------------------------------------------------
+# ../dj_blog/app_blogs/forms.py
+# ------------------------------------------------------------------
+
+from django import forms
+# from django.forms import ModelForm, Textarea
+# django.forms.ModelChoiceField             ChoiceField
+# django.forms.ModelMultipleChoiceField     MultipleChoiceField
+from .models import Entry, Tag, Topic, Post
+
+
+# ==================================================================
+# Topic:    id, topic, is_checked, is_active, date_beg, date_end, date_created, date_modified
+# Tag:      id, tag, is_checked, is_active, date_beg, date_end, date_created, date_modified
+# Post:     id, title, text, fk: topic, mm: tag, is_checked, is_active, date_beg, date_end, date_created, date_modified
+# Entry:    id, fk:post, text, is_checked, is_active, date_beg, date_end, date_created, date_modified
+# ==================================================================
+
+
+# ==================================================================
+# class XXXForm(forms.ModelForm):
+#     class Meta:
+#         model = XXX
+#         # fields = '__all__'
+#         # fields = ['name', 'text', 'is_checked', 'is_active', 'date_beg', 'date_end']
+#         fields = ['name', 'text']
+#         lables = {'name': 'Name', 'text': 'Text'}
+#         widgets = {'text': forms.Textarea(attrs={'cols': 80, 'rows': 5})}
+
+# -----------------------------------------------
+# Post: id, title, text, fk: topic, mm: tag, is_checked, is_active, date_beg, date_end, date_created, date_modified
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+# -----------------------------------------------
+# Entry:    id, fk:post, text, is_checked, is_active, date_beg, date_end, date_created, date_modified
+
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = '__all__'
+
+# -----------------------------------------------
+# Tag:  id, tag, is_checked, is_active, date_beg, date_end, date_created, date_modified
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+# -----------------------------------------------
+# Topic:    id, topic, is_checked, is_active, date_beg, date_end, date_created, date_modified
+
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model = Topic
+        fields = '__all__'
+
+
+# ----------------------------------------------------------------------------
+# {{ form.as_table }}   will render them as table cells wrapped in <tr> tags
+# {{ form.as_p }}       will render them wrapped in <p> tags
+# {{ form.as_ul }}      will render them wrapped in <li> tags
